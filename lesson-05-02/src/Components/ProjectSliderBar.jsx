6197dba2575e488628b9e06ada33592a4db1f5ee
borscs <andras.borscs@opticruiter.com>
2025-02-11 18:25:36 +0100

    lesson-06

--- lesson-05-02/src/Components/ProjectSliderBar.jsx
+++ lesson-05-02/src/Components/ProjectSliderBar.jsx
@@ -1,5 +1,5 @@
 
-export default function ProjectSliderBar({onStartAddProject}) {
+export default function ProjectSliderBar({onStartAddProject, projects, onSelectedProject}) {
 	return (
 		<aside className="w-1/3 px-8 py-16 bg-stone-800 text-stone-50 md:w-72 rounded-r-xl">
 			<h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -10,6 +10,15 @@ export default function ProjectSliderBar({onStartAddProject}) {
 					+ Add Project
 				</button>
 			</div>
+			<ul className="mt-8">
+				{projects.map((item, index) => (
+					<li key={index}>
+						<button onClick={() => onSelectedProject(item.id)}  className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+							{item.title}
+						</button>
+					</li>
+				))}
+			</ul>
 		</aside>
 	)
 }
